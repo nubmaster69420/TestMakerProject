ce396c60aebb809594cf4168e12ecddc6e2a6d3a
George <vasilevskygeorge@gmail.com>
2021-11-08 18:38:21 +0700

    Except hook add

--- QTengine.py
+++ QTengine.py
@@ -309,8 +309,13 @@ class MainWindow(QMainWindow):
             self.success_window()
 
 
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MainWindow()
     ex.show()
+    sys.excepthook = except_hook
     sys.exit(app.exec())
